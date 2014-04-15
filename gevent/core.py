c168f705e306e83e8e1b95ac2837e73f799ffbf8
Denis Bilenko <denis.bilenko@gmail.com>
2014-04-15 19:54:33 +0200

    core.py: import _flags_to_int

--- gevent/core.py
+++ gevent/core.py
@@ -2,7 +2,7 @@ from gevent.hub import PYPY
 
 if PYPY:
     from gevent.corecffi import *
-    from gevent.corecffi import __all__
+    from gevent.corecffi import __all__, _flags_to_int
 else:
     from gevent.corecext import *
-    from gevent.corecext import __all__
+    from gevent.corecext import __all__, _flags_to_int
